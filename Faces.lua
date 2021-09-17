6391a39ee2a147ea47cec0b1f1c3573218632661
UncleJBus <90570395+UncleJBus@users.noreply.github.com>
2021-09-16 20:51:12 -0700

    Update Faces.lua

--- Faces.lua
+++ Faces.lua
@@ -1,16 +1,17 @@
 return {
 	happy = {
-		7477949953;
-		7477954089;
-		7477954891;
-		7477950792;
-		7478870306;
-		165386907;
+		7477949953;	--Andrew Epicson
+		7477954089; --Epic Face HD
+		7477950792;	--Lua Epic Face
+		7478870306;	--Roblox Epic Face Remastered HD 4k RTX Blueray
+		165386907;	--United States Epic Face
+		6825580489;	--Epic Face w/ Shades B)
 
 	},
 
 	sad = {
-		7477956443;
+		7477956443;	--Sad Epic FAce
+		3325089469;	--Epic FAce but scary 8-bit D=
 	},
 
 	normal = {
