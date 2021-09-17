8323d4351d1810537e8dcd9432dfb80487f193cc
UncleJBus <90570395+UncleJBus@users.noreply.github.com>
2021-09-17 12:28:51 -0700

    Update Faces.lua

--- Faces.lua
+++ Faces.lua
@@ -6,12 +6,37 @@ return {
 		7478870306;	--Roblox Epic Face Remastered HD 4k RTX Blueray
 		165386907;	--United States Epic Face
 		6825580489;	--Epic Face w/ Shades B)
+		6825579082;	--Rainbow Epic Face
+		34718124;	--Orange Epic Face
+		66310360;	--Pink Epic Face
+		77515058;	--Green Epic Face
+		51107661;	--Gray/Moon Epic Face
+		37193113;	--Epic Pumpkin Face
+		58421290;	--Epic Striped Face
+		46252323;	--Epic Snake Face
+		82145892;	--Epic Camo Face
+		45536337;	--CLASSIC Epic Face
+		37193206;	--Epic Cookie Face
+		62906031;	--Epic Flame Face
+		657555283;	--Epic Demoman Face
+		67959228;	--Epic Soldier Face
+		51107487;	--Epic Tropical Face
+		1003674477;	--XD
+		33933778;	--3D Epic Face
+		442682462;	--Epic Italian Mustache Face
+		46984516;	--Epic Water Face
+		38847782;	--Wooden Epic Face
+		1494159658;	--Transparent Epic Face
+		12128337;	--Epic Joker Face
 
 	},
 
 	sad = {
-		7477956443;	--Sad Epic FAce
+		7477956443;	--Sad Epic Face
 		3325089469;	--Epic FAce but scary 8-bit D=
+		36375018;	--Skeptical Epic Face
+		51516144;	--Epic Stoner Face
+		437166761;	--D=
 	},
 
 	normal = {
